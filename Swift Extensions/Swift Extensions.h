8ba53372a04ca4f1920a56e0a78b95df37f33bb9
Wim Van Renterghem <vrwim@me.com>
2015-04-02 19:37:00 +0200

    Initial Commit

---
+++ Swift Extensions/Swift Extensions.h
@@ -0,0 +1,19 @@
+//
+//  Swift Extensions.h
+//  Swift Extensions
+//
+//  Created by Wim Van Renterghem on 02/04/15.
+//  Copyright (c) 2015 Wim Van Renterghem. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Swift Extensions.
+FOUNDATION_EXPORT double Swift_ExtensionsVersionNumber;
+
+//! Project version string for Swift Extensions.
+FOUNDATION_EXPORT const unsigned char Swift_ExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swift_Extensions/PublicHeader.h>
+
+
